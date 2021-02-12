a49ef6c44885490b2c9cc39fbf5d18f26bc01e56
Mohammed Musthafa <muhammed.mustafa@emvigotech.com>
2021-02-13 03:09:00 +0530

    minor fixes with custom font

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
-import React, { useState } from 'react'
-import { Button } from "antd"
-import "antd/dist/antd.css"
-import ReactQuill from "react-quill";
-import "quill/dist/quill.snow.css"
+import React, { useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import { Button } from "antd";
+
+import "react-quill/dist/quill.snow.css";
+import "antd/dist/antd.css";
+import "./styles.css";
+
 import useAutoSave from "./customHooks/useAutoSave";
+import CustomToolBar from "./components/CustomToolBar";
 
-function App() {
+
+
+const Size = Quill.import("formats/size");
+Size.whitelist = ["extra-small", "small", "medium", "large"];
+Quill.register(Size, true);
+
+const Font = Quill.import("formats/font");
+Font.whitelist = [
+  "arial",
+  "comic-sans",
+  "open-sans",
+  "courier-new",
+  "georgia",
+  "helvetica",
+  "lucida"
+];
+Quill.register(Font, true);
+
+function App(){
   const [text, setText] = useState("");
-
   const saving = useAutoSave(text);
 
+  const modules = {
+    toolbar: {
+      container: "#toolbar"
+    }
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color"
+  ];
+
   return (
-    <div>
+    <div className="text-editor">
+      <CustomToolBar />
       <ReactQuill
         value={text}
-        onChange={value => setText(value)}
+        onChange={(value) => setText(value)}
+        modules={modules}
+        formats={formats}
       />
-      { saving ?
-        <Button type="primary" loading disabled>Saving</Button>
-        :
-        <Button type="primary">Save</Button> }
+
+      { saving ? 
+      <Button type="primary" loading disabled>Saving</Button>
+      :
+      <Button type="primary">Save</Button> 
+    }
     </div>
-  )
+  );
 }
 
 export default App
